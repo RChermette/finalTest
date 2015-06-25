c81e6682c42af4bbe610fe50b35d7e2e7c128fd8
Your Name <p1415460@IUTDOUAB2PC11.univ-lyon1.fr>
2015-06-25 10:39:21 +0200

    Sixth commit

---
+++ FinalTest/Nombres.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace FinalTest
+{
+    public class Nombres
+    {
+        private readonly IEnumerable<KeyValuePair<string, int>> _keyValuePairs;
+
+        public Nombres(IEnumerable<KeyValuePair<string, int>> keyValuePairs)
+        {
+            _keyValuePairs = keyValuePairs;
+        }
+
+        public IEnumerable<int> NombresPairs
+        {
+            get { return _keyValuePairs.Where(pair => pair.Value % 2 == 0).Select(pair => pair.Value); }
+        }
+    }
+}
