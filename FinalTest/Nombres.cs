29c41a76776cfa7672c08a664f0f9decf225f8d2
Your Name <p1415460@IUTDOUAB2PC11.univ-lyon1.fr>
2015-06-25 10:51:40 +0200

    autre commit

--- FinalTest/Nombres.cs
+++ FinalTest/Nombres.cs
@@ -18,5 +18,15 @@ namespace FinalTest
         {
             get { return _keyValuePairs.Where(pair => pair.Value % 2 == 0).Select(pair => pair.Value); }
         }
+
+        public String TexteNombresImpairs
+        {
+            get
+            {
+                return _keyValuePairs.Where(x => x.Value%2 != 0)
+                    .OrderBy(x => x.Value)
+                    .Select(x => x.Key).Aggregate((i, j) => i + ", " + j);
+            }
+        }
     }
 }
